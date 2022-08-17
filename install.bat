e879511bc16e88e5b39b3b0ffa6ef161151e81e6
GoodBoyNeon <93624576+GoodBoyNeon@users.noreply.github.com>
2022-08-17 08:42:31 +0545

    Fix typo

--- install.bat
+++ install.bat
@@ -1,4 +1,4 @@
-@echo "Install dependencies, please be patient, this may take upto [few seconds - 5min] according to your internet performence"
+@echo "Install dependencies, please be patient, this may take upto [few seconds - 5min] according to your internet performance"
 npm i
 @echo "Installations completed, Open start.bat to enjoy with your bot"
 @echo "Consider subscribing to https://www.youtube.com/CodingWithNithish"
